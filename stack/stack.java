342e935c3643cb15548ed8f864c4a4fe64bd04a2
lkwoung <whrjzm123@gmail.com>
2023-01-20 17:33:25 +0900

    스택 구현

---
+++ stack/stack.java
@@ -0,0 +1,56 @@
+import java.util.EmptyStackException;
+
+public class stack<E> {
+    private int gv_top = 0;
+    private int gv_size = 0;
+    private E[] gv_array;
+
+    public stack(int capacity) {
+        if (capacity < 0){
+            throw new IllegalArgumentException("Illegal Capacity: " + capacity);
+        }
+        this.gv_array = (E[]) new Object[capacity];
+        this.gv_size = capacity;
+    }
+
+    public stack() {
+        this(10);
+    }
+
+    public synchronized E pop() {
+        E object = this.peek();
+        gv_top--;
+        return object;
+    }
+
+    public synchronized void push(E item) {
+        if (gv_top == gv_size) {
+            throw new FullStackException();
+        }
+        gv_array[gv_top++] = item;
+    }
+
+    public synchronized E peek() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return gv_array[gv_top-1];
+    }
+
+    public synchronized int search(E item) {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        for (int i = gv_top-1; i > -1; i--) {
+            if (item.equals(gv_array[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean isEmpty() {
+        return gv_top == 0 ? true : false;
+    }
+
+}
--- stack/test.java
+++ stack/test.java
@@ -1,5 +1,12 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        stack<Integer> lv_stack_a = new stack<Integer>();
+        lv_stack_a.push(1);
+        lv_stack_a.push(2);
+        lv_stack_a.push(3);
+        lv_stack_a.push(4);
+        System.out.println(lv_stack_a.pop());
+        System.out.println(lv_stack_a.peek());
+        System.out.println(lv_stack_a.search(8));
     }
 }
